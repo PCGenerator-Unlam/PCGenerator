72b1396900f6fdf8e71ab9a9f3a802889530138f
walter bruno <walter_bruno21091987@hotmail.com>
2023-07-04 11:41:36 -0300

    11_39

--- app/src/main/java/com/example/pcgenerator/models/Ram.kt
+++ app/src/main/java/com/example/pcgenerator/models/Ram.kt
@@ -2,7 +2,8 @@ package com.example.pcgenerator.models
 
 class Ram(nombreMarca: String,
 
-          val cantidadDeMemoria:Int): Componentes(TipoDeComponente.RAM,nombreMarca){
+          val cantidadDeMemoria:Int,precio:Double=0.0,
+          image: Int? =null,): Componentes(TipoDeComponente.RAM,nombreMarca,image,precio){
     override fun descripcion(int: Int): String {
         return "${cantidadDeMemoria.toString()} Gb"
     }
