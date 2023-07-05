eb5debd2ad65c145dc6978bb417188d8297e650c
walter bruno <walter_bruno21091987@hotmail.com>
2023-07-05 12:12:37 -0300

    5_7_1016

--- app/src/main/java/com/example/pcgenerator/models/Pc.kt
+++ app/src/main/java/com/example/pcgenerator/models/Pc.kt
@@ -1,4 +1,4 @@
 package com.example.pcgenerator.models
 
-class Pc(val tipoDePc:TipoDePc=TipoDePc.LIBRE,var prosesador:Procesador?=null,var memoria:Ram?=null, var mother:Mother?=null,var placa:TarjetaGrafica?=null) {
+class Pc(val tipoDePc:TipoComputadora=TipoComputadora.LIBRE, var prosesador:Procesador?=null, var memoria:Ram?=null, var mother:Mother?=null, var placa:TarjetaGrafica?=null) {
 }
--- app/src/main/java/com/example/pcgenerator/models/TipoDePc.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pcgenerator.models
-
-enum class TipoDePc {
-    PROGRAMADOR,
-    DISEÑADOR,
-    OFICINA,
-    LIBRE
-
-}
